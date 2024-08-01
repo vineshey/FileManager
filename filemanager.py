439c8ab02318fc501f873b3462f171d8c2bafaab
vineshey <166842520+vineshey@users.noreply.github.com>
2024-08-01 15:12:40 +0530

    Add files via upload
    create folders of same name as it is seen in code(eg: SFX,Images,Videos etc).
    specify the paths correctly, crosscheck if required.
    run the script and you will start seeing the documents move

---
+++ filemanager.py
@@ -0,0 +1,94 @@
+from os import scandir, rename
+from os.path import splitext, exists, join
+from shutil import move
+from time import sleep
+
+import logging
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# Fill in the below paths with actual directories on your system
+source_dir = "C:\\Users\\ACER\\Downloads"
+dest_dir_sfx = "C:\\Users\\ACER\\Downloads\\SFX"
+dest_dir_music = "C:\\Users\\ACER\\Downloads\\Music"
+dest_dir_video = "C:\\Users\\ACER\\Downloads\\Videos"
+dest_dir_image = "C:\\Users\\ACER\\Downloads\\Images"
+dest_dir_documents = "C:\\Users\\ACER\\Downloads\\Documents"
+
+image_extensions = [".jpg", ".jpeg", ".png", ".gif", ".webp"]
+video_extensions = [".mp4", ".avi", ".mov"]
+audio_extensions = [".mp3", ".wav", ".aac"]
+document_extensions = [".doc", ".docx", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
+
+def make_unique(dest, name):
+    filename, extension = splitext(name)
+    counter = 1
+    while exists(f"{dest}/{name}"):
+        name = f"{filename}({str(counter)}){extension}"
+        counter += 1
+    return name
+
+def move_file(dest, entry, name):
+    if exists(f"{dest}/{name}"):
+        unique_name = make_unique(dest, name)
+        oldName = join(dest, name)
+        newName = join(dest, unique_name)
+        rename(oldName, newName)
+    move(entry, dest)
+
+class MoverHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        with scandir(source_dir) as entries:
+            for entry in entries:
+                name = entry.name
+                self.check_audio_files(entry, name)
+                self.check_video_files(entry, name)
+                self.check_image_files(entry, name)
+                self.check_document_files(entry, name)
+
+    def check_audio_files(self, entry, name):
+        for audio_extension in audio_extensions:
+            if name.endswith(audio_extension) or name.endswith(audio_extension.upper()):
+                if entry.stat().st_size < 10_000_000 or "SFX" in name:
+                    dest = dest_dir_sfx
+                else:
+                    dest = dest_dir_music
+                move_file(dest, entry, name)
+                logging.info(f"Moved audio file: {name}")
+
+    def check_video_files(self, entry, name):
+        for video_extension in video_extensions:
+            if name.endswith(video_extension) or name.endswith(video_extension.upper()):
+                move_file(dest_dir_video, entry, name)
+                logging.info(f"Moved video file: {name}")
+
+    def check_image_files(self, entry, name):
+        for image_extension in image_extensions:
+            if name.endswith(image_extension) or name.endswith(image_extension.upper()):
+                move_file(dest_dir_image, entry, name)
+                logging.info(f"Moved image file: {name}")
+
+    def check_document_files(self, entry, name):
+        for documents_extension in document_extensions:
+            if name.endswith(documents_extension) or name.endswith(documents_extension.upper()):
+                move_file(dest_dir_documents, entry, name)
+                logging.info(f"Moved document file: {name}")
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = source_dir
+    event_handler = MoverHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    print("finished moving")
+
+    try:
+        while True:
+            sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
